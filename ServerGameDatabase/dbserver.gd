62187e970f87f86f5d6b1893601097180df3a510
funjobie <funjobie@hotmail.com>
2024-02-05 09:00:47 +0100

    initial structure of version check and update, added version and user table

--- ServerGameDatabase/dbserver.gd
+++ ServerGameDatabase/dbserver.gd
@@ -16,6 +16,13 @@ enum LogLevel
 {
 	INFO, WARN, ERR
 }
+const CURRENT_DB_VERSION : int = 1
+
+const DB_VERSION_TABLE_NAME = "db_version_table"
+const DB_VERSION_TABLE_VERSION_COLUMN_NAME = "version"
+const USERS_TABLE_NAME = "users_table"
+const USERS_TABLE_USERID_COLUMN_NAME = "user_id"
+const USERS_TABLE_PUBLIC_KEY_COLUMN_NAME = "public_key"
 
 func log_and_print(severity : LogLevel, text) -> void:
 	#todo consider also creating a log file?
@@ -31,6 +38,50 @@ func log_and_print(severity : LogLevel, text) -> void:
 	if text_edit.text.length() > visible_log_length:
 		text_edit.text = text_edit.text.right(visible_log_length / 2)
 	pass
+
+func create_tables_if_not_existing(db) -> void:
+	#var table_dict : Dictionary = Dictionary()
+	#table_dict["id"] = {"data_type":"int", "primary_key": true, "not_null": true}
+	#table_dict["name"] = {"data_type":"text", "not_null": true}
+	#table_dict["age"] = {"data_type":"int", "not_null": true}
+	#table_dict["address"] = {"data_type":"char(50)"}
+	#table_dict["salary"] = {"data_type":"real"}
+	
+	var db_version_table_dict : Dictionary = Dictionary()
+	db_version_table_dict[DB_VERSION_TABLE_VERSION_COLUMN_NAME] = {"data_type":"int", "primary_key": true, "not_null": true}
+	db.create_table(DB_VERSION_TABLE_NAME, db_version_table_dict)
+	var db_version_table_row_dict : Dictionary = Dictionary()
+	db_version_table_row_dict[DB_VERSION_TABLE_VERSION_COLUMN_NAME] = CURRENT_DB_VERSION
+	db.insert_row(DB_VERSION_TABLE_NAME, db_version_table_row_dict)
+	
+	var users_table_dict : Dictionary = Dictionary()
+	users_table_dict[USERS_TABLE_USERID_COLUMN_NAME] = {"data_type":"text", "primary_key": true, "not_null": true}
+	users_table_dict[USERS_TABLE_PUBLIC_KEY_COLUMN_NAME] = {"data_type":"text", "not_null": true}
+	db.create_table(USERS_TABLE_NAME, users_table_dict)
+	
+	pass
+
+func upgrade_tables(db, oldVersion, newVersion) -> void:
+	log_and_print(LogLevel.INFO, "upgrading db from version " + str(oldVersion) + " to version " + str(newVersion))
+	log_and_print(LogLevel.ERR, "upgrade from version " + str(oldVersion) + " to version " + str(newVersion) + " is not yet supported!")
+	pass
+
+const NO_VERSION_IN_DB : int = -1
+const INCONSISTENT_VERSION_IN_DB : int = -2
+
+func GetVersionFromDB(db) -> int:
+	
+	var select_condition : String = ""
+	var selected_array : Array = db.select_rows(DB_VERSION_TABLE_NAME, select_condition, [DB_VERSION_TABLE_VERSION_COLUMN_NAME])
+	
+	print("condition: " + select_condition)
+	print("result: {0}".format([str(selected_array)]))
+	
+	if selected_array.size() == 0:
+		return NO_VERSION_IN_DB
+	elif selected_array.size() > 1:
+		return INCONSISTENT_VERSION_IN_DB
+	return selected_array[0][DB_VERSION_TABLE_VERSION_COLUMN_NAME]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -54,9 +105,26 @@ func _ready() -> void:
 		log_and_print(LogLevel.INFO, "success opening database at " + OS.get_user_data_dir() + "/database.db")
 	else:
 		log_and_print(LogLevel.ERR, "error opening database, please check folder " + OS.get_user_data_dir() + " allows creating files.")
-		
-		
 	
+	var localVersion : int = GetVersionFromDB(db)
+	if localVersion == INCONSISTENT_VERSION_IN_DB:
+		log_and_print(LogLevel.ERR, "database has inconsistent versions; startup is stopped. this is not supposed to happen, consider reporting as a bug and erasing the db. ")
+		return
+	elif localVersion == NO_VERSION_IN_DB:
+		log_and_print(LogLevel.INFO, "db does not contain version number yet -> creating table structures")
+		create_tables_if_not_existing(db)
+		#todo
+	elif localVersion < CURRENT_DB_VERSION:
+		log_and_print(LogLevel.INFO, "db is outdated -> updating structure")
+		upgrade_tables(db, localVersion, CURRENT_DB_VERSION)
+		#todo
+	elif localVersion > CURRENT_DB_VERSION:
+		log_and_print(LogLevel.ERR, "database has greater version than expected; startup is stopped. rolling back to previous software versions is currently not supported.")
+		return
+	else: #same version
+		log_and_print(LogLevel.INFO, "db version is up to date")
+	
+
 	pass # Replace with function body.
 
 
